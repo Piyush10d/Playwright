c6ee96bdca27605fb55165bdbfd09742e7881fac
Piyush <piyushdhawas96@gmail.com>
2025-06-30 23:08:46 +0530

    test

--- tests/myFile.spec.ts
+++ tests/myFile.spec.ts
@@ -1,1 +1,18 @@
-//my changes
+import { test, expect } from '@playwright/test';
+
+test('login ', { tag: '@fast'}, async ({ page }) => {
+  await page.goto('https://github.com/');
+  await page.getByRole('button', { name: 'Product' }).click();
+  await page.getByRole('link', { name: 'Actions Automate any workflow' }).click();
+  await expect(page.getByRole('heading', { name: 'Automate your workflow from' })).toBeVisible();
+  await expect(page.locator('#hero-section-brand-heading')).toContainText('Automate your workflow from idea to production');
+  await page.getByRole('link', { name: 'Issues' }).click();
+  await expect(page.locator('#hero-section-brand-heading')).toContainText('Project planning for developers');
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.close();
+  //New changes in imdea
+  await page.getByRole('link', { name: 'Issues' }).click();
+  await expect(page.locator('#hero-section-brand-heading')).toContainText('Project planning for developers');
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.close();
+});
